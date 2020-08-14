75ffa230d22dc194127b216786cfb5a2b1e479bb
mocobk <mailmzb@qq.com>
2020-08-14 16:43:50 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="mitmdump",
-    version="1.0.1",
+    version="1.0.2",
     author="mocobk",
     author_email="mailmzb@qq.com",
     description="以编程的方式运行 mitmproxy",
@@ -17,11 +17,20 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mocobk/mitmdump",
     packages=['mitmdump'],
+    install_requires=['mitmproxy'],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    extras_require={
+        ':sys_platform == "win32"': [
+            "pydivert>=2.0.3,<2.2",
+        ],
+        ':python_version == "3.6"': [
+            "dataclasses>=0.7",
+        ]
+    }
 )
-
-install_requires = ['mitmproxy']
